7ca1c01fc410d92ea17f905ead829903ba8add2b
svkaizoku <svignesh81235@gmail.com>
2022-05-27 23:52:23 +0530

    removed controller code from main class

--- src/main/java/com/samplewebapp/cicd/GithubCicdApplication.java
+++ src/main/java/com/samplewebapp/cicd/GithubCicdApplication.java
@@ -6,12 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class GithubCicdApplication {
-	@GetMapping("/welcome")
-	public String welcome() {
-		return "Hello World!";
-	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdApplication.class, args);
 	}
